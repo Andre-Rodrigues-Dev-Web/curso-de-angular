e7b01da1c6314da6c15c3a1c4f47cd782d91787a
André Rodrigues <79713707+Andre-Rodrigues-Dev-Web@users.noreply.github.com>
2024-09-07 16:58:24 -0300

    feat: adicionados arquivos

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # curso-de-angular
- Esse curso é para quem deseja aprender sobre o Angular
+## Esse curso é para quem deseja aprender sobre o Angular 
+
+### Pré-requisitos
+- Navegador atualizado (Recomendavel Firefox, Chrome e navegador Edge)
+- Editor de código (Fiquem a vontade para escolher seu editor)
+- Instalar o NodejS (recomendavel versão 18)
+- Ter um mterminal instalado (Hyper, ou GitBash)
+
+### Versão a ser usada em primeiro momento no projeto
+npm install -g @angular/cli@15
---
+++ loja-top/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { OlaMundoComponent } from './shared/components/ola-mundo/ola-mundo.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    OlaMundoComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
